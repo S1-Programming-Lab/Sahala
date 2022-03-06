5d3bc83f94c41ebf4600634af37c948ff20766eb
sahalabakker <94426450+sahalabakker@users.noreply.github.com>
2022-03-06 08:49:34 +0530

    Python program

---
+++ Array operations.py
@@ -0,0 +1,12 @@
+array = [10, 20, 30, 40]
+print("max=", max(array))
+print("min=", min(array))
+print("sum=", sum(array))
+print("length=", len(array))
+list = [30, 10, 60, 25, 45]
+print("array=", array)
+print("list=", list)
+print("sorted array=", sorted(list))
+print("All elements of the array are greater than 10", all(list))
+print("any of element in the array list is equals to 25", any(list))
+
